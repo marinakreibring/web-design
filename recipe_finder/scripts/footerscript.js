325f40f9ed003be522e8c6cf38b74b125688e2eb
marinakreibring <timarina1997@yahoo.com>
2024-12-07 18:51:29 +0100

    user input validation

--- recipe_finder/scripts/footerscript.js
+++ recipe_finder/scripts/footerscript.js
@@ -1,4 +1,4 @@
-// script for copyright in footer
-document.querySelector("#currYear").textContent = new Date().getFullYear();
-document.querySelector("#updated").textContent = document.lastModified;
-// script for current date in header
+// script for footer
+//document.querySelector("#currYear").textContent = new Date().getFullYear();
+//document.querySelector("#updated").textContent = document.lastModified;
+
